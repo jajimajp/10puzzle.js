6bc31695df5c92ca5fb33bc1c611c76d6dec53ed
Yajima <119467077+jajimajp@users.noreply.github.com>
2024-11-30 15:28:35 +0900

    Calculate user input

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,5 +1,40 @@
-const url = new URL(document.location.href);
+import { Parser } from './parser.js';
+import { tokenize } from './lexer.js';
 
 const root = document.querySelector('#root');
-root.innerHTML = `<p>The URL hash is ${url.hash}.`;
+root.innerHTML = `
+<p id="status"></p>
+<input type="text" id="textinput" />
+<input type="button" id="button" value="Check" />
+`;
+
+const status = document.querySelector('#status');
+const textinput = document.querySelector('#textinput');
+const button = document.querySelector('#button');
+
+/** @param {string} statusText */
+const renderStatus = (statusText) => {
+  status.textContent = statusText;
+};
+
+const url = new URL(document.location.href);
+renderStatus(`The URL hash is ${url.hash}`);
+
+button.addEventListener('click', () => {
+  console.log('textinput.value', textinput.value);
+  check(textinput.value);
+});
+
+/** @param {string} input */
+const check = (input) => {
+  try {
+    const tokens = tokenize(input);
+    const ast = new Parser(tokens).parse();
+    const result = ast.eval();
+
+    renderStatus(`${input} => ${result}`);
+  } catch(e) {
+    console.error(e);
+  }
+};
 
